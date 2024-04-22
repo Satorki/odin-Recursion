c12a242f5180b464214910d1d77348a3cfe5e28c
satork <sebastian.kedzier@gmail.com>
2024-04-22 18:36:13 +0200

    solution

--- recursion.js
+++ recursion.js
@@ -1,51 +1,65 @@
-function sumTo(n) {
-  let sum = 0;
-  for (let i = n; i >= 0; i--) {
-    sum += i;
-  }
-  return sum;
-}
-
-function recSumTo(n) {
-  if (n == 1) {
-    return 1;
+function fibs(n) {
+  let arr = [0, 1];
+  if (n === 0) {
+    arr = [];
+  } else if (n === 1) {
+    arr = [0];
   } else {
-    return n + recSumTo(n - 1);
+    for (let i = 1; i < n - 1; i++) {
+      arr.push(arr[i - 1] + arr[i]);
+    }
   }
+  return arr;
 }
+//console.log(fibs(8));
 
-function recFractional(n) {
-  if (n == 1) {
-    return 1;
+function rFibs(n) {
+  if (n === 0) {
+    return [0];
+  } else if (n === 1) {
+    return [0, 1];
   } else {
-    return n * recFractional(n - 1);
+    let arr = rFibs(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
   }
 }
+//console.log(rFibs(8));
 
-function collatz(n) {
-  if (n == 1) {
-    return 0;
-  } else if (n % 2 == 0) {
-    return 1 + collatz(n / 2);
-  } else {
-    return 1 + collatz(3 * n + 1);
+function merge(leftArr, rightArr) {
+  const output = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < leftArr.length && rightIndex < rightArr.length) {
+    const leftEl = leftArr[leftIndex];
+    const rightEl = rightArr[rightIndex];
+
+    if (leftEl < rightEl) {
+      output.push(leftEl);
+      leftIndex++;
+    } else {
+      output.push(rightEl);
+      rightIndex++;
+    }
   }
+
+  return [...output, ...leftArr.slice(leftIndex), ...rightArr.slice(rightIndex)];
 }
 
-function power(x, n) {
-  if (n == 0) {
-    return 1;
-  } else {
-    return x * power(x, n - 1);
+//console.log(merge([3,5],[5,4]));
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
+
+  const middleIndex = Math.floor(arr.length / 2);
+  const leftArr = arr.slice(0, middleIndex);
+  const rightArr = arr.slice(middleIndex);
+
+  return merge(mergeSort(leftArr), mergeSort(rightArr));
 }
 
-function all(arr) {
-  if (arr.length === 0) {
-    return 1;
-  } else {
-    return arr.shift() * all(arr);
-  }
-}
+//console.log(mergeSort([105, 79, 100, 110]));
 
-console.log(all([1, 2, 3]));
